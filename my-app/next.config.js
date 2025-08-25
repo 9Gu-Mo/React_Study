8749aa94dfc5106bbfe184c96f9a773fb6282559
wonmo <129805449+9Gu-Mo@users.noreply.github.com>
2025-08-25 13:48:36 +0900

    feat: build 버전 커밋

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/React_Study",
+  assetPrefix: "/React_Study/",
   images: {
     domains: ["cdn.jsdelivr.net"],
   },
