a1979e29b460f18e4ab6fc9b751f88ab4e406e59
wonmo <129805449+9Gu-Mo@users.noreply.github.com>
2025-08-26 10:18:57 +0900

    feat: 이미지 경로 cdn으로 변경

--- my-app/src/components/TopContent.tsx
+++ my-app/src/components/TopContent.tsx
@@ -5,7 +5,7 @@ export default function TopContent() {
     <>
       <Image
         className="content-top"
-        src="/images/bg/bg.jpg"
+        src="https://cdn.jsdelivr.net/gh/9Gu-Mo/React_Study@main/my-app/public/images/bg/bg.jpg"
         width={1920}
         height={1280}
         alt=""
